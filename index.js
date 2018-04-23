1d3cd8af781290c35f7b606bf1e32c118eadc88a
Leonardo do Espírito Santo <leonardo.santo@inventti.com.br>
2018-04-23 16:48:07 -0300

    Adicionando opção para selecionar tudo

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 import './scss/theme'
+
+$(function() {
+    $('#select-all').on('click', () => {
+        let checkboxes = $(':checkbox');
+        let checked = $(checkboxes[0]).is(':checked');
+
+        for(let i=0; i<checkboxes.length; i++) {
+            $(checkboxes[i]).prop('checked', !checked);
+        }
+    })
+});
